a098d40046153734844e25ae410a33302f2d0431
joke PC <jokesoundz@gmail.com>
2025-08-14 14:07:06 +0100

    added PlaylistComponent files [PC]

---
+++ Source/PlaylistComponent.h
@@ -0,0 +1,29 @@
+/*
+  ==============================================================================
+
+    PlaylistComponent.h
+    Created: 14 Aug 2025 2:06:02pm
+    Author:  PC
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+*/
+class PlaylistComponent  : public juce::Component
+{
+public:
+    PlaylistComponent();
+    ~PlaylistComponent() override;
+
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PlaylistComponent)
+};
